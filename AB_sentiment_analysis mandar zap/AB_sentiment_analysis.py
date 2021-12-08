9e38733cba0df4ee0096b3cbf439553013ff1874
Vinicius Schaedler Damin <viniciusdamin@usp.br>
2021-12-08 04:02:37 -0300

    Update old code

---
+++ AB_sentiment_analysis mandar zap/AB_sentiment_analysis.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import spacy
+from googletrans import Translator
+
+translator = Translator()
+#é preciso fazer python -m spacy download en_core_web_sm antes da proxi
+nlp=spacy.load("en_core_web_sm")
+
+
+# Com base em Minqing Hu and Bing Liu. "Mining and Summarizing Customer Reviews."
+#Criamos duas listas com palavras positivas e negativas
+pos_txt=open('positive-words.txt').read()
+pos=pos_txt[1282:].split("\n")
+
+neg_txt=open('negative-words.txt').read()
+neg=neg_txt[1285:].split("\n")
+
+
+#recebe um texto e retorna um dicionário com os substantivos e o sentimento deles
+def feature_sentiment(Frase):
+    
+    text=translator.translate(Frase,src='pt', dest='en').text
+
+    sent_dict = dict()
+    sentence = nlp(text)
+    opinion_words = neg + pos
+    debug = 0
+    for token in sentence:
+        # check if the word is an opinion word, then assign sentiment
+        if token.text in opinion_words:
+            sentiment = 1 if token.text in pos else -1
+            # if target is an adverb modifier (i.e. pretty, highly, etc.)
+            # but happens to be an opinion word, ignore and pass
+            if (token.dep_ == "advmod"):
+                continue
+            elif (token.dep_ == "amod"):
+                sent_dict[token.head.text] = sentiment
+            # for opinion words that are adjectives, adverbs, verbs...
+            else:
+                for child in token.children:
+                    # if there's a adj modifier (i.e. very, pretty, etc.) add more weight to sentiment
+                    # This could be better updated for modifiers that either positively or negatively emphasize
+                    if ((child.dep_ == "amod") or (child.dep_ == "advmod")) and (child.text in opinion_words):
+                        sentiment *= 1.5
+                    # check for negation words and flip the sign of sentiment
+                    if child.dep_ == "neg":
+                        sentiment *= -1
+                for child in token.children:
+                    # if verb, check if there's a direct object
+                    if (token.pos_ == "VERB") & (child.dep_ == "dobj"):                        
+                        sent_dict[child.text] = sentiment
+                        # check for conjugates (a AND b), then add both to dictionary
+                        subchildren = []
+                        conj = 0
+                        for subchild in child.children:
+                            if subchild.text == "and":
+                                conj=1
+                            if (conj == 1) and (subchild.text != "and"):
+                                subchildren.append(subchild.text)
+                                conj = 0
+                        for subchild in subchildren:
+                            sent_dict[subchild] = sentiment
+
+                # check for negation
+                for child in token.head.children:
+                    noun = ""
+                    if ((child.dep_ == "amod") or (child.dep_ == "advmod")) and (child.text in opinion_words):
+                        sentiment *= 1.5
+                    # check for negation words and flip the sign of sentiment
+                    if (child.dep_ == "neg"): 
+                        sentiment *= -1
+                
+                # check for nouns
+                for child in token.head.children:
+                    noun = ""
+                    if (child.pos_ == "NOUN") and (child.text not in sent_dict):
+                        noun = child.text
+                        # Check for compound nouns
+                        for subchild in child.children:
+                            if subchild.dep_ == "compound":
+                                noun = subchild.text + " " + noun
+                        sent_dict[noun] = sentiment
+                    debug += 1
+                    
+    #os aspects e os sentiments estão salvos em sent_dict, porém quero traduzir as keys do meu dicionário.
+    #crio um novo dicionário com as keys traduzidas:
+    #print(sent_dict)
+    
+    dic_sentimentos={}
+    for key in sent_dict:
+        chave=translator.translate(key,src='en', dest='pt').text
+        dic_sentimentos[chave]=sent_dict[key]
+    return dic_sentimentos
+
+
+# In[9]:
+
+
+print(feature_sentiment("Gostei do meu chefe mas odiei meu sálario"))
+
+
+# In[ ]:
+
+
+
+
---
+++ sentiment_analysis.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+#importar as bibliotecas necessárias
+import nltk
+import pickle
+from nltk.tokenize import word_tokenize
+from statistics import mode
+from nltk.classify import ClassifierI
+
+#primeiro temos que fazer pickle.load em todos os arquivos necessários
+words_features_f=open('words_features.pickle','rb')
+words_features=pickle.load(words_features_f)
+words_features_f.close()
+
+classifiers_str=['NaiveBayes_classifier',
+                 'MNB_classifier',
+                 'BernoulliNB_classifier',
+                 'SGDClassifier_classifier',
+                  #retirei 'SVC_classifier',
+                 'LinearSVC_classifier',
+                 'NuSVC_classifier']
+classifiers_list=[]
+for classifier in classifiers_str:
+    classifier_f=open(classifier+'.pickle','rb')
+    classifiers_list.append((pickle.load(classifier_f),classifier))
+
+NaiveBayes_classifier=classifiers_list[0][0]
+MNB_classifier=classifiers_list[1][0]
+BernoulliNB_classifier=classifiers_list[2][0]
+SGDClassifier_classifier=classifiers_list[3][0]
+#SVC_classifier=classifiers_list[4][0] da erro
+LinearSVC_classifier=classifiers_list[4][0]
+#NuSVC_classifier=classifiers_list[5][0]
+
+
+
+
+#Como não é possível fazer pickle.dump na instância de VoteClassifier:
+class VoteClassifier(ClassifierI):
+    def __init__(self, *classifiers):
+        #guarda os 8 classificadores
+        self.classifiers=classifiers
+          
+    def classify(self, features):
+        votes=[]
+        for c in self.classifiers:
+            v=c.classify(features)
+            #cada classificador vota uma vez
+            votes.append(v)
+        #o sentimento que é selecionado é o que aparece mais vezes (mais votado)
+        return mode(votes)
+    def confidence(self,features):
+        votes=[]
+        for c in self.classifiers:
+            v=c.classify(features)
+            votes.append(v)
+        choice_votes=votes.count(mode(votes))
+        conf=choice_votes/len(votes)
+        #temos também o valor de confiança de nossa predição
+        return conf
+voted_classifier=VoteClassifier(NaiveBayes_classifier,
+                                MNB_classifier,
+                                BernoulliNB_classifier,
+                                SGDClassifier_classifier,
+                                #retirei 'SVC_classifier',
+                                LinearSVC_classifier,
+                                LinearSVC_classifier,
+                                #NuSVC_classifier
+                                )
+
+
+
+
+
+#Como não é possível fazer pickle.dump na instância de VoteClassifier:
+class VoteClassifier(ClassifierI):
+    def __init__(self, *classifiers):
+        #guarda os 8 classificadores
+        self.classifiers=classifiers
+          
+    def classify(self, features):
+        votes=[]
+        for c in self.classifiers:
+            v=c.classify(features)
+            #cada classificador vota uma vez
+            votes.append(v)
+        #o sentimento que é selecionado é o que aparece mais vezes (mais votado)
+        return mode(votes)
+    def confidence(self,features):
+        votes=[]
+        for c in self.classifiers:
+            v=c.classify(features)
+            votes.append(v)
+        choice_votes=votes.count(mode(votes))
+        conf=choice_votes/len(votes)
+        #temos também o valor de confiança de nossa predição
+        return conf
+voted_classifier=VoteClassifier(NaiveBayes_classifier,
+                                MNB_classifier,
+                                BernoulliNB_classifier,
+                                SGDClassifier_classifier,
+                                #retirei 'SVC_classifier',
+                                LinearSVC_classifier,
+                                #NuSVC_classifier,NuSVC_classifier
+                                )
+
+
+#Definimos a função criada no documento 'Creating the algorithm'
+def find_features(document):
+    
+#temos que usar word_tokenize pois recebemos uma review em formato de string contento várias palavras.
+    words=word_tokenize(document)
+    features={}
+    #cada palavra das 7000 mais frequentes é avaliada se está ou não
+    #na review em questão, se estiver ela é marcada como True
+    for w in words_features:
+        features[w]=(w in words)
+    return features
+
+#importar um tradutor
+from googletrans import Translator
+translator = Translator()
+
+def sentiment(text):
+
+    feats=find_features(text)
+    
+    return (voted_classifier.classify(feats),voted_classifier.confidence(feats))
+
+
+#função que recebe uma string e retorna o sentimento dela
+def sentiment_pt(texto):
+    text=translator.translate(texto,src='pt', dest='en').text
+    return sentiment(text)
+
+
+# In[ ]:
+
+# %%
